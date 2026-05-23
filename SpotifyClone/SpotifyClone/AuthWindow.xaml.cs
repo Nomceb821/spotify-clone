2de1c35b533fb60c559fda8078e9839be582588b
Nomcebo821 <nomcebonkomo203@gmail.com>
2026-05-23 19:18:14 +0200

    Initial commit

---
+++ SpotifyClone/SpotifyClone/AuthWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SpotifyClone
+{
+    /// <summary>
+    /// Interaction logic for AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerWindow playerWindow = new PlayerWindow();
+
+            playerWindow.Show();
+
+            this.Close();
+        }
+        //For the eye to show the password
+        private bool isPasswordVisible = false;
+
+        private void ShowPasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isPasswordVisible)
+            {
+                VisiblePasswordTextBox.Text = PasswordTextBox.Password;
+
+                VisiblePasswordTextBox.Visibility = Visibility.Visible;
+
+                PasswordTextBox.Visibility = Visibility.Collapsed;
+
+                isPasswordVisible = true;
+            }
+            else
+            {
+                PasswordTextBox.Password = VisiblePasswordTextBox.Text;
+
+                PasswordTextBox.Visibility = Visibility.Visible;
+
+                VisiblePasswordTextBox.Visibility = Visibility.Collapsed;
+
+                isPasswordVisible = false;
+            }
+        }
+    }
+}
---
+++ SpotifyClone/SpotifyClone/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotifyClone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        // NAVIGATE TO PLAYER WINDOW
+        private void GetSpotifyButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+
+            authWindow.Show();
+
+            this.Close();
+        }
+    }
+}
